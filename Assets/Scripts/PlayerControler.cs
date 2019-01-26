a61bddd9b6a64de9446469f8cfc7a4c8e83e5a30
JC <ghostrorecon@gmail.com>
2019-01-26 13:30:47 -0800

    added moving player character, with temp sprite 00

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//By JC
+
+public class PlayerControler : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D rb; // attached
+	private Vector2 moveVelocity;
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> ();
+
+	}
+
+	void Update () {
+		Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")); // Input.GetAxis for smooth 
+		moveVelocity = moveInput.normalized * speed; //just input
+	}
+
+	void FixedUpdate(){
+		rb.MovePosition (rb.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
+
+
