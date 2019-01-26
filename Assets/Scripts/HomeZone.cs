dbda5eae6c0e5a94d002e7e9bc4679e1ad9d8a8a
Brian Egana <brianm.egana@gmail.com>
2019-01-26 11:26:58 -0800

    pushing again to reflect updated Unity version

---
+++ Assets/Scripts/HomeZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HomeZone : MonoBehaviour
+{
+    private string ENEMY_TAG = "Enemy";
+
+    public Text roundOverText;
+    private bool homeIsHit = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(ENEMY_TAG))
+        {
+            homeIsHit = true;
+            roundOverText.text = "Round Over";
+        }
+    }
+
+    private void Update()
+    {
+        if (homeIsHit && Input.anyKeyDown)
+        {
+            roundOverText.text = "";
+        }
+    }
+}
