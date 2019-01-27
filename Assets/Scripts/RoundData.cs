a9733bfcdafb42e9c1a5f9051d77a740ded001bd
Brian Egana <brianm.egana@gmail.com>
2019-01-27 13:12:41 -0800

    modified enemy spawn have rounds within one scene

---
+++ Assets/Scripts/RoundData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoundData", menuName = "ScriptableObjects/RoundData", order = 1)]
+public class RoundData : ScriptableObject
+{
+    public int intervalIndex;
+    public int numIntervals;
+
+    public int[] enemyGroupCounts;
+    public float[] spawnTimes;
+    public enum Direction { North, South, East, West, Random };
+    public Direction[] nextSpawnDirections;
+    public enum Enemy { SmallRat, BigRat, Hando, Slick, Fred };
+    public Enemy[] nextEnemiesToSpawn;
+}
