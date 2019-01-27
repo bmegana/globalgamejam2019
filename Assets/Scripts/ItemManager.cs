b2959ac6fde13a4f6cc1f430e4cfc10037dd7433
Brian Egana <brianm.egana@gmail.com>
2019-01-26 21:26:04 -0800

    removed gitignore rule for ignoring meta files

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemManager : MonoBehaviour
+{
+    public static ItemManager instance;
+    public ArrayList decorationsList; //TODO: integrate with looting script
+
+    public GameObject decorationsPanel;
+    public GameObject decorations;
+    public int maxNumDecorations;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            decorationsList = new ArrayList();
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void Start()
+    {
+        maxNumDecorations = decorations.transform.childCount;
+    }
+
+    public void DisplayDecorateMenu()
+    {
+        decorationsPanel.SetActive(true);
+    }
+
+    public void DropDecoration()
+    {
+        
+    }
+
+    public void UpdateDecorationList(ArrayList chosenLootList)
+    {
+        foreach (GameObject loot in chosenLootList)
+        {
+            DontDestroyOnLoad(loot);
+            decorationsList.Add(loot);
+        }
+    }
+}
