2e017cc04c10a6925bc7372bf14612635213d7e2
Brian Egana <brianm.egana@gmail.com>
2019-01-27 15:08:58 -0800

    added PlayerInventory file and ScriptableObjects

---
+++ Assets/Scripts/ScriptableObjects/Gun.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun", menuName = "ScriptableObjects/Guns", order = 1)]
+public class Gun : ScriptableObject {
+
+    public Color color;
+    public string gunName;
+    public float fireRate;
+    public float lineWidth;
+}
